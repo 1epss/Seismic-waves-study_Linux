8be90449fe4cb4471c569f8916e21cf99753e4f5
Natsu Kagami <nki@nkagami.me>
2023-09-08 12:09:49 +0200

    Compile hypoel automatically

--- flake.nix
+++ flake.nix
@@ -6,11 +6,24 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
+
+      hypoel = pkgs.stdenv.mkDerivation {
+        pname = "hypoel";
+        version = "0.0.0-old";
+        buildInputs = with pkgs; [ gfortran ];
+        nativeBuildInputs = with pkgs; [ glibc.static ];
+        src = ./hypoellipse/source;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp Hypoel $out/bin/hypoel
+        '';
+      };
     in
     with lib; {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           # List packages needed here
+          hypoel
           ## For compiling hypoellipse
           gfortran
           gnumake
